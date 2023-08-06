7b6a487967cc33bf29098085275e4ddaf2e7e415
jalfonzo412 <alfonzojose3494@gmail.com>
2023-08-05 21:23:02 -0400

    Crear el endpoint post

--- database/centro_adopcion.sql
+++ database/centro_adopcion.sql
@@ -22,3 +22,46 @@ ALTER TABLE animales
 ADD url_img TEXT;
 
 DESCRIBE animales;
+
+insert into animales(grupo, especie, raza, anios, peso_aprox, descricpion, url_img) values
+	(
+        'Mamifero', 
+        'Perro', 
+        'Dóberman',
+        1,
+        32.7,
+        'Preparate para enamorarte de este maravilloso perro que es una combinación única de elegancia, valentía y alegría',
+        'https://pixabay.com/static/frontend/3c346409d336d5f09a7f.svg'
+    ),
+    (
+        'Mamifero', 
+        'Perro', 
+        'Pastor Aleman',
+        0,
+        13.5,
+        '¡El pastor alemán, el perro todoterreno con actitud de superhéroe! Es el perro perfecto para aventuras emocionantes y travesuras divertidas, ¡un auténtico héroe peludo!',
+        'https://cdn.pixabay.com/photo/2019/11/09/20/59/german-shepherd-4614456_640.jpg'
+    ),
+    (
+        'Mamifero', 
+        'Gato', 
+        'Siberiano',
+        1,
+        3.8,
+        '¡El gato siberiano, el peludo trotamundos con estilo! Es el compañero perfecto para acurrucarse en invierno, ¡te mantendrá caliente y llenará tu vida de pura elegancia felina!',
+        'https://cdn.pixabay.com/photo/2020/08/11/18/45/cat-5480864_640.jpg'
+    ),
+    (
+        'Reptil', 
+        'Serpiente', 
+        'Serpiente del maíz',
+        3,
+        0.4,
+        '¡La serpiente del maíz, la reina de los reptiles con su patrón de escamas de maíz a la moda!',
+        'https://cdn.pixabay.com/photo/2016/06/09/15/51/snake-1446008_640.jpg'
+    );
+
+alter table animales
+change descricpion descripcion text; 
+
+select * from animales;
--- src/controllers/animals.controller.js
+++ src/controllers/animals.controller.js
@@ -1,6 +1,22 @@
+import { pool } from '../db.js';
+
 export const getAnimals = (req, res) => res.send('obteniendo animales')
 
-export const createAnimals = (req, res) => res.send('creando registro para el animal')
+export const createAnimals = async (req, res) => {
+  const {grupo, especie, raza, anios, peso_aprox, descripcion, url_img} = req.body
+  const [rows] = await pool.query('INSERT INTO animales (grupo, especie, raza, anios, peso_aprox, descripcion, url_img) VALUES (?, ?, ?, ?, ?, ?, ?)', 
+  [grupo, especie, raza, anios, peso_aprox, descripcion, url_img])
+  res.send({ 
+    id: rows.insertId,
+    grupo, 
+    especie, 
+    raza, 
+    anios, 
+    peso_aprox, 
+    descripcion,
+    url_img
+  })
+}
 
 export const updateAnimals = (req, res) => res.send('actualizando registro del animal')
 
