0db68ee02ed1a80145f89a046cc6742c9056a2ba
jalfonzo412 <alfonzojose3494@gmail.com>
2023-08-07 13:33:23 -0400

    Agregar la dependencia cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import animalsRoutes from './routes/animals.routes.js';
 import indexRoutes from './routes/index.routes.js';
 
 const app = express();
 
 app.use(express.json())
+app.use(cors());
 app.use(indexRoutes)
 app.use('/api', animalsRoutes)
 
